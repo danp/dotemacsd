edb1dd7d429eab47818c44d096d56be288493bcd
Dan Peterson <dpiddy@gmail.com>
2011-10-15 13:01:09 -0300

    Initial.

---
+++ init.el
@@ -0,0 +1,61 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(
+  starter-kit
+  starter-kit-bindings
+  starter-kit-ruby
+  markdown-mode
+  yaml-mode
+  coffee-mode
+  color-theme-ir-black
+  gist
+  go-mode
+  haml-mode
+  textmate
+  auto-indent-mode
+  maxframe
+  marmalade))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+(require 'color-theme-ir-black)
+(color-theme-ir-black)
+
+(server-mode 1)
+
+(require 'textmate)
+(textmate-mode 1)
+; reset the list of ignored things; previously included at least 'log'
+(setq *textmate-gf-exclude*
+   "/\\.|vendor|tmp|\\.xcodeproj|\\.nib|\\.framework|\\.app|\\.pbproj|\\.pbxproj|\\.xcode|\\.xcodeproj|\\.bundle|\\.pyc|\\.rbc")
+; reset this to include Gemfile
+(setq *textmate-project-roots*
+   '(".git" ".hg" "Rakefile" "Makefile" "Gemfile" "README" "build.xml"))
+
+(set-default-font "Menlo-12")
+
+(require 'maxframe)
+(setq mf-max-width 1600)
+(add-hook 'window-setup-hook 'maximize-frame t)
+
+; autoindent on yank-pop
+(dolist (command '(yank yank-pop))
+  (eval `(defadvice ,command (after indent-region activate)
+           (and (not current-prefix-arg)
+                (member major-mode '(emacs-lisp-mode lisp-mode
+                                                     clojure-mode    scheme-mode
+                                                     haskell-mode    ruby-mode
+                                                     rspec-mode      python-mode
+                                                     c-mode          c++-mode
+                                                     objc-mode       latex-mode
+                                                     plain-tex-mode))
+                (let ((mark-even-if-inactive transient-mark-mode))
+                  (indent-region (region-beginning) (region-end) nil))))))
