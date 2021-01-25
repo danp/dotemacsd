6d43f695799857a884ac8048fecb0e5a645852b0
Dan Peterson <danp@danp.net>
2021-01-25 11:58:11 -0400

    wip

--- init.el
+++ init.el
@@ -72,6 +72,17 @@
   :bind (("C-s" . swiper)
          ("C-r" . swiper)))
 
+(use-package ivy-xref
+  :init
+  ;; xref initialization is different in Emacs 27 - there are two different
+  ;; variables which can be set rather than just one
+  (when (>= emacs-major-version 27)
+    (setq xref-show-definitions-function #'ivy-xref-show-defs))
+  ;; Necessary in Emacs <27. In Emacs 27 it will affect all xref-based
+  ;; commands other than xref-find-definitions (e.g. project-find-regexp)
+  ;; as well
+  (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
+
 (use-package markdown-mode)
 
 (use-package magit
