c0817827d5de30d056f64f58f63af91b7abf1707
Dan Peterson <danp@danp.net>
2021-01-30 19:05:07 -0400

    wip

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ivy-xref markdown-mode eglot flymake-shellcheck flycheck protobuf-mode yaml-mode rust-mode elm-mode enh-ruby-mode enhanced-ruby-mode find-file-in-project rainbow-delimiters swiper ivy diminish yasnippet company exec-path-from-shell go-mode magit use-package)))
+   '(caddyfile-mode ivy-xref markdown-mode eglot flymake-shellcheck flycheck protobuf-mode yaml-mode rust-mode elm-mode enh-ruby-mode enhanced-ruby-mode find-file-in-project rainbow-delimiters swiper ivy diminish yasnippet company exec-path-from-shell go-mode magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
