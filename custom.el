e743c94ed43458dce61f32b357852bcb5ab419d2
Dan Peterson <danp@danp.net>
2023-04-28 13:00:16 -0300

    custom update

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("919fabfc5cb6165ce07b9d8668f78fe75fe8bd08566006bc87513c29b4f34ade" default))
  '(package-selected-packages
-   '(acme-theme color-identifiers-mode fish-mode git-link ag lsp-mode caddyfile-mode ivy-xref markdown-mode flymake-shellcheck flycheck protobuf-mode yaml-mode rust-mode elm-mode enh-ruby-mode enhanced-ruby-mode find-file-in-project rainbow-delimiters swiper ivy diminish yasnippet company exec-path-from-shell go-mode magit use-package))
+   '(editorconfig lsp-tailwindcss acme-theme color-identifiers-mode fish-mode git-link ag lsp-mode caddyfile-mode ivy-xref markdown-mode flymake-shellcheck flycheck protobuf-mode yaml-mode rust-mode elm-mode enh-ruby-mode enhanced-ruby-mode find-file-in-project rainbow-delimiters swiper ivy diminish yasnippet company exec-path-from-shell go-mode magit use-package))
  '(warning-suppress-log-types '((comp) (comp)))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
